8015564a41cbdf3f7eb558da0ebf4f0a5a3114ff
Tales Cunha <tvac@cin.ufpe.br>
2023-12-06 19:45:39 -0300

    (new): added ionic/storage-angular

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+const STORAGE_KEY = 'myData';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    console.log('init');
+    await this.storage.create();
+    console.log('created');
+  }
+
+  getData(){
+    return this.storage.get(STORAGE_KEY) || [];
+  }
+
+  async addData(data){
+    const storaged = await this.storage.get(STORAGE_KEY) || [];
+    storaged.push(data);
+    return this.storage.set(STORAGE_KEY, storaged);
+  }
+
+  async removeData(id){
+    const storaged = await this.storage.get(STORAGE_KEY) || [];
+    storaged.splice(id, 1);
+    return this.storage.set(STORAGE_KEY, storaged);
+  }
+}

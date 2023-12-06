7b23939b3f1fab104e14d3b2e36231fb717647fb
Tales Cunha <tvac@cin.ufpe.br>
2023-12-06 14:32:55 -0300

    (new): display filtered escola

--- src/app/pages/consultas/consultas.page.ts
+++ src/app/pages/consultas/consultas.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EscolaService } from 'src/app/services/escolas.service';
+import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-consultas',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultas.page.scss'],
 })
 export class ConsultasPage implements OnInit {
-
-  constructor() { }
-
+  escolas = [];
+  constructor(private escolaService: EscolaService, private loadingCtrl: LoadingController,private route:ActivatedRoute) {}
   ngOnInit() {
+    this.carregarEscolaFiltrada();
+  }
+  /*
+   Funcao que carrega as escolas filtradas pelo nome da entidade e codigo da entidade passados como parametro
+  */
+  async carregarEscolaFiltrada() {
+    const loading = await this.loadingCtrl.create({
+      message: 'Carregando escolas...',
+      spinner: 'crescent',
+    });
+    const coEntidade = this.route.snapshot.paramMap.get('coEntidade');
+    const noEntidade = this.route.snapshot.paramMap.get('noEntidade');
+    await loading.present();
+    this.escolaService.getEscolasFiltered(Number(coEntidade),noEntidade).subscribe(res => {
+      loading.dismiss();
+      this.escolas = res;
+    });
   }
 
 }

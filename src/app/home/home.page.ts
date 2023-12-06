3a0b87e7cd0b544fccaab3abd65ddaf1a595bd8c
Tales Cunha <tvac@cin.ufpe.br>
2023-12-06 14:28:40 -0300

    (update): chenge layout of Homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router} from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-  
+  constructor(private route: Router) {}
+  /*
+   Função que é chamada quando o botão é clicado e redireciona para a página de consulta
+  */
+  onButtonClick() {
+    let inputvalue1 = (<HTMLInputElement>document.getElementById("input1")).value;
+    let inputvalue2 = (<HTMLInputElement>document.getElementById("input2")).value;
+    this.route.navigate(['/consultas', inputvalue1, inputvalue2]);
+  }
 }

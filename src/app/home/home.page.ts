6d7268f4491ea3f53e18babc715cf011acaac635
Tales Cunha <tvac@cin.ufpe.br>
2023-12-07 13:16:27 -0300

    (update): add restriction to consultar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,8 +11,19 @@ export class HomePage {
    Função que é chamada quando o botão é clicado e redireciona para a página de consulta
   */
   onButtonClick() {
-    let inputvalue1 = (<HTMLInputElement>document.getElementById("input1")).value;
-    let inputvalue2 = (<HTMLInputElement>document.getElementById("input2")).value;
+    let inputElement1 = <HTMLInputElement>document.getElementById("input1");
+    let inputElement2 = <HTMLInputElement>document.getElementById("input2");
+  
+    let inputvalue1 = inputElement1.value;
+    let inputvalue2 = inputElement2.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
+  
+    if (!inputvalue1 || !inputvalue2) {
+      alert('Por favor, preencha ambos os campos.');
+      return;
+    }
+  
+    inputElement2.value = inputvalue2;
+  
     this.route.navigate(['/consultas', inputvalue1, inputvalue2]);
   }
 }

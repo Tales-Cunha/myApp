ba1721cda8c40bc3f4f19bbe51902398d6730e7e
Tales Cunha <tvac@cin.ufpe.br>
2023-12-07 14:56:32 -0300

    (new): added alerts to consultas

--- src/app/pages/consultas/consultas.page.ts
+++ src/app/pages/consultas/consultas.page.ts
@@ -16,7 +16,8 @@ export class ConsultasPage implements OnInit {
   constructor(private escolaService: EscolaService,
      private loadingCtrl: LoadingController,
      private route:ActivatedRoute,
-     private dataService: DataService,) {
+     private dataService: DataService,
+     private alertController: AlertController) {
     this.loadFavoritos();
     }
   ngOnInit() {
@@ -25,6 +26,16 @@ export class ConsultasPage implements OnInit {
   /*
    Funcao que carrega as escolas filtradas pelo nome da entidade e codigo da entidade passados como parametro
   */
+   async presentAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: message,
+      buttons: ['OK']
+    });
+  
+    await alert.present();
+  }
+
   async carregarEscolaFiltrada() {
     const loading = await this.loadingCtrl.create({
       message: 'Carregando escolas...',
@@ -36,6 +47,13 @@ export class ConsultasPage implements OnInit {
     this.escolaService.getEscolasFiltered(Number(coEntidade),noEntidade).subscribe(res => {
       loading.dismiss();
       this.escolas = res;
+      if (this.escolas.length === 0) {
+        this.presentAlert('Nenhuma escola encontrada', 'Não há escolas que correspondam aos seus critérios de pesquisa.');
+      }
+    }, error => {
+      loading.dismiss();
+      console.error(error);
+      this.presentAlert('Erro', 'Ocorreu um erro ao carregar as escolas. Por favor, tente novamente mais tarde.')
     });
   }
 
